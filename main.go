3df3905ae44850399ac9aeeb1d8854f607d034b9
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:40:33 +0530

    using project-planton apis

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	grafanakubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/kubernetes/grafanakubernetes/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/grafana-kubernetes-pulumi-module/pkg"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/grafanakubernetes"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		stackInput := &grafanakubernetes.GrafanaKubernetesStackInput{}
+		stackInput := &grafanakubernetesv1.GrafanaKubernetesStackInput{}
 
 		if err := stackinput.LoadStackInput(ctx, stackInput); err != nil {
 			return errors.Wrap(err, "failed to load stack-input")
--- pkg/main.go
+++ pkg/main.go
@@ -1,13 +1,13 @@
 package pkg
 
 import (
+	grafanakubernetesv1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/kubernetes/grafanakubernetes/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/grafanakubernetes"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Resources(ctx *pulumi.Context, stackInput *grafanakubernetes.GrafanaKubernetesStackInput) error {
+func Resources(ctx *pulumi.Context, stackInput *grafanakubernetesv1.GrafanaKubernetesStackInput) error {
 	//create kubernetes-provider from the credential in the stack-input
 	_, err := pulumikubernetesprovider.GetWithKubernetesClusterCredential(ctx,
 		stackInput.KubernetesCluster, "kubernetes")
